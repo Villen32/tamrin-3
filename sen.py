955cf8b7563c4ab76c9ad5b001ced83f71f9e4de
Villen <psychookiller72@gmail.com>
2025-11-21 13:18:09 +0330

    Create sen.py

---
+++ sen.py
@@ -0,0 +1,10 @@
+sal = int(input("age? : "))
+
+rooz = sal * 365
+saat = rooz * 24
+daghighe = saat * 60
+saniye = daghighe * 60
+
+print("sene to be saat:", saat)
+print("sene to be daghighe:", daghighe)
+print("sene to be saniye:", saniye)
